96c7a1841591cdca0d9122f5d28b5a6906f67c55
AlphaHIT <prateekpriyadarshi1328@gmail.com>
2021-11-01 20:25:38 +0530

    Set State In Depth

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,43 @@ class CartItem extends React.Component {
             qty: 1,
             img:''
         }
+        //this.increaseQuantity = this.increaseQuantity.bind(this);
     }
+
+    increaseQuantity = () => {
+        //this.state.qty += 1;
+        console.log('this', this.state);
+        //form1
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+        
+        //setState form2
+        this.setState((prevState)=>{
+            return{
+                qty:prevState.qty +1
+            }
+        }
+        )
+    }
+    decreaseQuantity = () => {
+        //object destructuring
+        //in case if our quantity is 0
+        const { qty } = this.state;
+        if (qty === 0){
+            return;
+        }
+
+        //this.state.qty += 1;
+        console.log('this', this.state);
+        //form1
+        this.setState({
+             qty: this.state.qty - 1
+         });
+        
+        
+    }
+
     render() {
         const {price, title, qty} = this.state;
         return(
@@ -24,9 +60,23 @@ class CartItem extends React.Component {
                     <div className="cart-item-action">
                         {/*Buttons */}
                     </div>
-                    <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
-                    <img alt="decrease" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2734/premium/2734848.png?token=exp=1635774713~hmac=0e212b7fbb3e6b0e8186910d859153f4"/>
-                    <img alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1635774801~hmac=10106ca3b78738d244986e4d46192797"/>
+
+                    <img alt="increase" 
+                    className="action-icons"
+                     src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                     onClick={this.increaseQuantity}
+                     />
+
+                    <img alt="decrease" 
+                    className="action-icons" 
+                    src="https://cdn-icons.flaticon.com/png/512/2734/premium/2734848.png?token=exp=1635774713~hmac=0e212b7fbb3e6b0e8186910d859153f4"
+                    onClick={this.decreaseQuantity}
+                    />
+                    
+                    <img alt="delete" 
+                    className="action-icons" 
+                    src="https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1635774801~hmac=10106ca3b78738d244986e4d46192797"
+                    />
 
                     
                 </div>
